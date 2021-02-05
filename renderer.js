26c3197d54205bf3cc527892f48d7917e1d6979f
MoonWhiteZqy <70324806+MoonWhiteZqy@users.noreply.github.com>
2021-02-05 22:15:18 +0800

    can open folder

--- renderer.js
+++ renderer.js
@@ -1,6 +1,25 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const fs = require('fs')
+const {ipcRenderer} = require('electron')
+// console.log(fs)
+// read_file = function(){
+//     fs.readdir('D:/文件/漫画/镇魂街', function(err, files){
+//         if(err){
+//             return console.error(err);
+//         }
+//         files.forEach(function(file){
+//             console.log(file);
+//         })
+//     })
+// }
+read_file = function(){
+    ipcRenderer.send('open-directory-dialog', 'openDirectory');
+    ipcRenderer.once('selectedItem', function(event, path){
+        if(path.length > 0){
+            console.log(path[0]);
+        }
+        else{
+            console.log('not dir chosen');
+        }
+    });
+
+}
